d0abb9494a419e0574e1d067bd7d4c416fb115d2
Juzba <Juzba88@gmail.com>
2025-06-28 20:42:40 +0200

    di update

--- NavigateWpf/App.xaml.cs
+++ NavigateWpf/App.xaml.cs
@@ -1,14 +1,43 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NavigateWpf.View;
+using NavigateWpf.ViewModel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace NavigateWpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; } = default!;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+
+            mainWindow.Show();
+            mainWindow.Content = new Page1View();
+            mainWindow.DataContext = new Page1ViewModel(); 
+        }
+
+        
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+
+
+
+
     }
 
 }
---
+++ NavigateWpf/ViewModel/Page1ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NavigateWpf.ViewModel
+{
+    public class Page1ViewModel : ObservableObject
+    {
+        public Page1ViewModel()
+        {
+            PageForward = new RelayCommand(PageFrw);  
+            PageBackward = new RelayCommand(PageBck);  
+        }
+
+        public IRelayCommand PageForward { get; }
+        private void PageFrw()
+        {
+            MessageBox.Show("PageForward");
+        }
+
+        public IRelayCommand PageBackward { get; }
+        private void PageBck()
+        {
+            MessageBox.Show("PageBackward");
+        }
+
+    }
+}
